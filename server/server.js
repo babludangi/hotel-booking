abc5b82c177971abadef92ac281a828b967fc103
Bablu Dangi <babludangi972@gmail.com>
2025-07-06 16:44:23 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import "dotenv/config";
+import cors from 'cors';
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
+
+
+connectDB();
+
+const app = express();
+app.use(cors());
+
+
+
+// middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// Api to listen clerk webhooks
+app.use("/api/clerk", clerkWebhooks);
+
+app.get('/', (req, res) => {
+  res.send('API is working');
+});
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
