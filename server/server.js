d4fc647a807015cf23a155d0e520850f3ab63105
Bablu Dangi <babludangi972@gmail.com>
2025-07-08 16:42:42 +0530

    stripe added

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ import { clerkMiddleware } from "@clerk/express";
 import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
 import roomRouter from "./routes/roomRoutes.js";
-//import bookingRouter from "./routes/bookingRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 import connectCloudinary from "./configs/cloudinary.js";
-//import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
+import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
 
 connectDB();
 connectCloudinary();
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors()); // Enable Cross-Origin Resource Sharing
 
 // API to listen to Stripe Webhooks
-//app.post("/api/stripe",express.raw({ type: "application/json" }),stripeWebhooks);
+app.post("/api/stripe",express.raw({ type: "application/json" }),stripeWebhooks);
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -31,7 +31,7 @@ app.get("/", (req, res) => res.send("API is working"));
 app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
 app.use("/api/rooms", roomRouter);
-//app.use("/api/bookings", bookingRouter);
+app.use("/api/bookings", bookingRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
